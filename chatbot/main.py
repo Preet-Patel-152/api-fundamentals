4002ea5faa679635406e9f880b9d789163ea2030
Preet-Patel-152 <preetpatel7@hotmail.com>
2026-01-09 19:00:11 -0600

    updated

--- chatbot/main.py
+++ chatbot/main.py
@@ -22,7 +22,8 @@ def get_bot_reply(user_message):
         try:
             response = client.chat.completions.create(
                 model="gpt-3.5-turbo",
-                messages=[{"role": "user", "content": user_message}]
+                messages=[
+                    {"role": "user", "content": "act as a top level advisor and anser the flooing qution " + user_message}]
             )
             return response.choices[0].message.content
         except Exception as e:
